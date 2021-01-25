96be79dff6b7c6e68c9fae552448540b4cfb26f3
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-25 00:39:17 -0600

    core module - url

---
+++ reference/url_demo.js
@@ -0,0 +1,34 @@
+const url = require('url');
+// const URL = require('url').URL;
+/* NOTE: if you are using v6 (LTS), line 1 gives errors,
+*  if you get an error saying, TypeError: URL is not a constructor, 
+*  comment line 1, and uncomment line 2 */
+
+const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active');
+
+// Serialized URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
+
+// Host (root domain) - gets the port
+// http://mywebsite.com:8000/hello.html?id=100&status=active
+console.log(myUrl.host);
+
+// Hostname (does not get port)
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search);
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Add param
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams);
+
+// Loop through params
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`));
