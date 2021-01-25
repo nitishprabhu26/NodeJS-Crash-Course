cf5d8ee6673d65afdd585de8e1dad0e179766fd7
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-25 09:25:59 -0600

    core module - http

---
+++ reference/http_demo.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+// Create server object
+http
+  .createServer((req, res) => {
+    // Write response
+    res.write('Hello World');
+    res.end();
+  })
+  .listen(5000, () => console.log('Server running...'));
+
+//   open http://localhost:5000/
